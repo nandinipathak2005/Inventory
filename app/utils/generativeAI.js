6c9695b33401f66b0054001ea8136f07631bd3ab
Nandini Manoj Pathak <166193813+nandinipathak2005@users.noreply.github.com>
2024-09-01 15:57:10 +0530

    Added Required files

--- app/utils/generativeAI.js
+++ app/utils/generativeAI.js
@@ -1,15 +1,22 @@
-// generativeAI.js
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import YOUR_API_KEY from "@/apikey";
 
-const genAI = new GoogleGenerativeAI(YOUR_API_KEY);
+const apiKey = process.env.GEMINI_API_KEY;
+
+
+const genAI = new GoogleGenerativeAI(apiKey);
 
 export const fetchRecipeSuggestions = async (prompt) => {
   try {
+    // Get the generative model
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    
+    // Generate content based on the prompt
     const result = await model.generateContent(prompt);
+    
+    // Extract and return the response text
     const response = await result.response;
     const text = await response.text();
+    
     return text;
   } catch (error) {
     console.error('Error generating content:', error);
